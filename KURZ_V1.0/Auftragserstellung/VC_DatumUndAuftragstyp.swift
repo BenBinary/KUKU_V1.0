d5547ab4454dcaf4c51203b69804196e7c01ba69
Benedikt Kurz <benediktkurz@benedikts-imac-3.local>
2019-03-12 13:00:01 +0100

    Änderung von KUKU in KURZ

--- KURZ_V1.0/Auftragserstellung/VC_DatumUndAuftragstyp.swift
+++ KURZ_V1.0/Auftragserstellung/VC_DatumUndAuftragstyp.swift
@@ -24,8 +24,6 @@ class VC_DatumUndAuftragstyp: UIViewController {
     @IBAction func sgAuftragstypChanged(_ sender: UISegmentedControl) {
         
         
-        if sg_Auftragstyp.
-        
         
     }
     
